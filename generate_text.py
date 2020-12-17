0f32bdbc3d9a95146ba2d311258e206d080adf69
Christian Doucette <cdoucette826@gmail.com>
2020-12-16 19:56:17 -0500

    added reformatting for text

--- generate_text.py
+++ generate_text.py
@@ -1,9 +1,36 @@
 from  collections import Counter
 import json
+import re
 import torch
 import torch.nn as nn
 import numpy as np
 import lstm_class
+import preprocess
+
+#=======================================#
+#           Reformat Function           #
+#=======================================#
+
+def add_formatting(list_of_ids, id2word, should_capitalize):
+    words = [id2word[id] for id in list_of_ids]
+    for i in range(1, len(words)):
+        prev = words[i-1]
+        cur  = words[i]
+
+
+        if (prev in [".", "!", "?"]) or (cur in should_capitalize and should_capitalize[cur]):
+            if cur != "said":
+                words[i] = cur.capitalize()
+
+    string = ' '.join(words[1:])
+    string = re.sub('Mr ', 'Mr. ', string)             # Changes Mr back to Mr.
+    string = re.sub(r'\s([,.!?:;])', r'\1', string)    # Removes sapce before punctuation
+    #string = re.sub(r"( \.| \,| \;| \:| \!| \?)", lambda x: f'{x.group(1)}', string)   # Removes extra space in front of punctuation
+
+    return string
+
+
+
 
 
 #===========================================#
@@ -12,6 +39,9 @@ import lstm_class
 
 with open('trained_model/word_to_id.json') as json_file:
     word_to_id = Counter(json.load(json_file))
+
+with open('trained_model/always_capitalized.json') as json_file:
+    always_capitalized = json.load(json_file)
 
 id_to_word = ["_"] + [word for word, index in word_to_id.items()]
 
@@ -59,9 +89,10 @@ def predict_next_id(network, ids_list, batch_size=1):
 #===========================================#
 
 n = 9
-num_sentences = 20
-num_paragraphs = 3
-seed_text = ""
+num_sentences = 3
+num_paragraphs = 1
+user_input = ""
+seed_text = preprocess.parse_and_clean(user_input)
 
 
 
@@ -87,5 +118,6 @@ for j in range(0, num_paragraphs):
             num_periods += 1
 
 
-    predicted_string = ' '.join([id_to_word[id] for id in ids[1:]])
+    #predicted_string = ' '.join(add_[id_to_word[id] for id in ids[1:]])
+    predicted_string = add_formatting(ids, id_to_word, always_capitalized)
     print(predicted_string)
